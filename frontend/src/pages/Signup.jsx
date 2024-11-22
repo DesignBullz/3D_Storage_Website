3b49988c072c96457191d191d1a9774999e67c82
jasmeetsingh23 <js2365536@gmail.com>
2024-11-22 12:00:34 +0530

    fwefwe'

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -264,7 +264,7 @@ function Signup() {
               type="submit"
               className="bg-gradient-to-r from-pink-500 to-purple-500 text-white px-6 py-2 rounded-md w-full hover:bg-gradient-to-r hover:from-pink-600 hover:to-purple-600 transition-all duration-300"
             >
-              Sign Up
+              Register
             </button>
           </form>
         </div>
